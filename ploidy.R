a12f63133ef47521d9682500500959ffbf7697b0
eilishmcmaster <eilish.summer@gmail.com>
2022-12-19 14:11:34 +1100

    added infividual sample hists

--- ploidy.R
+++ ploidy.R
@@ -8,7 +8,48 @@ counts2 <- read_dart_counts_csv_faster('ZierObco/dart_raw/Report_DZ22-7321_SNPco
                                        minAlleleCount=1, 
                                        minGenotypeCount=0)
 
-# plot histograms of the readcound data for all samples 
+
+# plot a histogram for each sample in dms_zod
+
+test2 <- count_subsetter(dms_zod, counts2, 0.05)
+
+tr <-  t(test2$c1)
+o <- lapply(split(tr,rownames(tr)), as.list)
+
+tr2 <-  t(test2$c2)
+o2 <- lapply(split(tr2,rownames(tr2)), as.list)
+
+nn <- mergeLists_internal(o, o2)
+
+minor <- lapply(nn, sapply, function(x) min(x)/sum(x))
+a <- do.call(rbind, minor) #make matrix
+major <- lapply(nn, sapply, function(x) max(x)/sum(x))
+b <- do.call(rbind, major) #make matrix
+c <- cbind(a,b)
+
+# only for six samples at a time
+par(mfrow = c(4, 5), mai=c(0.5,0.2,0.2,0.2))  # Set up a 2 x 2 plotting space
+
+# Create the loop.vector (all the columns)
+loop.vector <- 1:nrow(c)
+
+for (i in loop.vector) { # Loop over loop.vector
+  
+  # store data in column.i as x
+  x <- c[i,]
+  
+  # Plot histogram of x
+  hist(x,breaks=20,
+       main = paste(rownames(c)[i]),
+       xlab = "",#"MAF reads/ total reads",
+       ylab="",
+       xlim = c(0, 1))
+}
+
+hist(c)
+
+
+# plot histograms of the readcound data for all samples at the same time
 # run by species dms
 # minor allele frequency 0.05
 par(mfrow = c(2, 2),mai=c(0.5,0.5,0.2,0.2)) 
