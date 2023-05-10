9e889f6454bb51f8a958366c2c13d9af2e6fcdab
eilishmcmaster <eilish.summer@gmail.com>
2023-05-10 12:31:37 +1000

    update splitstree and add in new functions

--- 6 splitstree and svdquartets.R
+++ 6 splitstree and svdquartets.R
@@ -3,10 +3,10 @@
 # Splitstree ###################################################################
 # exporting data to be viewed in splitstree desktop gui 
 library(RSplitsTree)
-# d5_no0 <- as.data.frame(dms[["gt"]]) %>% mutate_all(~ifelse(is.na(.x), mean(.x, na.rm = TRUE), .x)) # calculated previously
-d5_splitstree <-d5_no0
-rownames(d5_splitstree) <- paste(rownames(d5_no0), dms$meta$analyses[,"sp"], sep=" ") # make leaf names include species
-splitstree(dist(d5_splitstree), paste0(species,'/outputs/all_zieria_nexus_file.nex'))
+
+d5_splitstree <- as.matrix(dist(dms$gt, diag=TRUE)) # dist object
+rownames(d5_splitstree) <- paste(rownames(dms$gt), dms$meta$analyses[,"sp"], dms$meta$analyses[,"genetic_group"], sep=" ") # make leaf names include species
+splitstree(dist(d5_splitstree), paste0(species,'/outputs/all_pultenaea_nexus_file.nex'))
 
 # SVDquartets ###################################################################
 # export data to calculate ML trees in SVDquartet
